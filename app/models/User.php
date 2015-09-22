a426afa15d09b13cd0c510f4c52792589bc7b640
hyy <376246826@qq.com>
2015-09-22 10:56:39 +0800

    添加User model

--- app/models/User.php
+++ app/models/User.php
@@ -19,68 +19,27 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password');
+	protected $hidden = [
+		'password',
+		'permissions',
+		'activation_code',
+		'activated_at',
+		'last_login',
+		'persist_code',
+		'reset_password_code',
+		'first_name',
+		'last_name'
+	];
 
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
-
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
-
-	/**
-	 * Get the token value for the "remember me" session.
-	 *
-	 * @return string
-	 */
-	public function getRememberToken()
-	{
-		return $this->remember_token;
-	}
-
-	/**
-	 * Set the token value for the "remember me" session.
-	 *
-	 * @param  string  $value
-	 * @return void
-	 */
-	public function setRememberToken($value)
-	{
-		$this->remember_token = $value;
-	}
-
-	/**
-	 * Get the column name for the "remember me" token.
-	 *
-	 * @return string
-	 */
-	public function getRememberTokenName()
-	{
-		return 'remember_token';
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
-
-
+	protected $fillable = [
+		'user_id',
+		'login_account',
+		'password',
+		'status',
+		'user_type',
+		'remark_code',
+		'remark_code',
+		'created_at',
+		'updated_at'
+	];
 }
