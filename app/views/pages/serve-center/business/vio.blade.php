1e0b1b7048008f6b9dc348f33a4dfb9582d88f8e
jv-juven <juven-jv@foxmail.com>
2015-09-23 11:48:16 +0800

    文件名字修改

--- app/views/pages/serve-center/business/vio.blade.php
+++ app/views/pages/serve-center/business/vio.blade.php
@@ -6,11 +6,11 @@
 
 @section("css")
 	@parent
-	<link rel="stylesheet" type="text/css" href="/dist/css/pages/serve/business/violation.css">
+	<link rel="stylesheet" type="text/css" href="/dist/css/pages/serve-center/business/violation.css">
 @stop
 
 @section("left-nav")
-	@include("components.left-nav.violation-left-nav")
+	@include("components.left-nav.serve-left-nav")
 @stop
 
 @section("right-content")
