90b5a6cf3eb5a2fa1510178f49d749b48ff26c90
hyy <376246826@qq.com>
2015-09-25 17:43:00 +0800

    补充分页组件

--- app/views/pages/message-center/message/base.blade.php
+++ app/views/pages/message-center/message/base.blade.php
@@ -38,25 +38,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="paginate-wrap clearfix">
-                <div class="clk-rd-wrap">
-                    <ul class="page-list clearfix">
-                        <li class="page-item"><</li>
-                        <li class="page-item active">1</li>
-                        <li class="page-item">2</li>
-                        <li class="page-item">3</li>
-                        <li class="page-item">4</li>
-                        <li class="page-item">5</li>
-                        <li class="page-item">></li>
-                    </ul>
-                </div>
-                <div class="ipt-rd-wrap">
-                    <form class="clearfix" method="GET" action="">
-                        <input type="text" name="page" class="ipt-page">
-                        <input type="submit" value="GO" class="ipt-sbm">
-                    </form>
-                </div>
-            </div>
+            @include('components.pagination')
         </div>
     </div>
 </div>
