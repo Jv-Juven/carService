93b455936d301a8c78b1c279f4b1465fb9a1de37
hyy <376246826@qq.com>
2015-09-25 16:43:34 +0800

    路由修改&逻辑修改

--- app/controllers/NoticeController.php
+++ app/controllers/NoticeController.php
@@ -1,15 +1,5 @@
 <?php
 
 class NoticeController extends BaseController{
-
-	//通知的详细内容
-	public function detail()
-	{
-		$notice_id = Input::get('notice_id');
-		$notice = Notice::find($notice_id);
-		if(!isset($notice))
-			return View::make('errors.missing');
-
-		return View::make('errors.missing')->with(array('notice'=>$notice));
-	}
+    
 }
