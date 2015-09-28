0004c1e34354d3089a869ad55a54f427803a23e7
hyy <376246826@qq.com>
2015-09-28 15:26:43 +0800

    调整feedback样式

--- app/views/pages/message-center/feedback/index.blade.php
+++ app/views/pages/message-center/feedback/index.blade.php
@@ -18,36 +18,45 @@
 
 @section('right-content')
 <div class="fb-wrap">
-    <form method="POST" action="" class="fb-form" tartet="fb-form-result">
+    <form method="POST" action="" class="fb-form" target="fb-form-result">
         <div class="fb-item fb-types clearfix">
             <div class="fb-label">反馈类型:</div>
             <div class="rt-wrap">
                 <div class="types-wrap clearfix">
-                    <input type="button" value="资讯" class="fb-type fb-type-act">
-                    <input type="button" value="建议" class="fb-type">
-                    <input type="button" value="投诉" class="fb-type">    
+                    <label class="type-checkbox">
+                        <input name="type" type="radio" class="cbox-clk">
+                        <span>资讯</span>
+                    </label>
+                    <label class="type-checkbox">
+                        <input name="type" type="radio" class="cbox-clk">
+                        <span>建议</span>
+                    </label>
+                    <label class="type-checkbox">
+                        <input name="type" type="radio" class="cbox-clk">
+                        <span>投诉</span>
+                    </label>
                 </div>
             </div>
         </div>
         <div class="fb-item fb-title clearfix">
             <div class="fb-label">标题:</div>
             <div class="rt-wrap">
-                <input type="text" class="fb-input">
+                <input name="title" type="text" class="fb-input">
             </div>
         </div>
         <div class="fb-item fb-content clearfix">
             <div class="fb-label">内容:</div>
             <div class="rt-wrap">
-                <textarea name="" id="" class="fb-input"></textarea>
+                <textarea name="content" id="" class="fb-input"></textarea>
             </div>
         </div>
         <div class="fb-btns">
             <div class="btns-wrap">
-                <input type="button" value="重置" class="fb-reset">
+                <input type="reset" value="重置" class="fb-reset">
                 <input type="submit" vlaue="提交" class="fb-submit">
             </div>
         </div>
     </form>
-    <iframe id="fb-form-result"></iframe>
+    <iframe name="fb-form-result" id="fb-form-result"></iframe>
 </div>
 @stop
