37d2682c3dcfc63651aa86f0617a7f7120fdf1c5
hyy <376246826@qq.com>
2015-09-30 16:55:27 +0800

    完成消息中心模块 --- 测试通过

--- app/views/pages/message-center/message/detail.blade.php
+++ app/views/pages/message-center/message/detail.blade.php
@@ -16,25 +16,13 @@
 <div class="message-wrap">
     <div class="message-body">
         <div class="message-content">
-            <h2 class="message-title">驾驶证过期后可能会出现的三种状态</h2>
+            <h2 class="message-title">{{{ $notice->title }}}</h2>
             <p class="message-info">
                 <span>发布日期:</span>
-                <span class="info-date">2015-09-12 01:15:12</span>
+                <span class="info-date">{{{ $notice->created_at }}}</span>
             </p>
             <div class="content-wrap">
-                <p>请驾驶员注意，正常情况下：</p>
-                <p>第一次领取驾驶证的有效期，是6年</p>
-                <p>第二次换证后，驾驶证的有效期，是10年</p>
-                <p>第三次换证后，才是长期使用的证件</p>
-                <p>驾驶证扣分查询系统提醒广大驾驶员，要在驾驶证快到期钱的90天内，前往车管所换证</p>
-                <p>而驾驶证超期后可能会出现下面三种状态：</p>
-                <img src="/images/common/qrcode.png">
-                <p>请驾驶员注意，正常情况下：</p>
-                <p>第一次领取驾驶证的有效期，是6年</p>
-                <p>第二次换证后，驾驶证的有效期，是10年</p>
-                <p>第三次换证后，才是长期使用的证件</p>
-                <p>驾驶证扣分查询系统提醒广大驾驶员，要在驾驶证快到期钱的90天内，前往车管所换证</p>
-                <p>而驾驶证超期后可能会出现下面三种状态：</p>
+                {{{ $notice->content }}}
             </div>
         </div>
     </div>
