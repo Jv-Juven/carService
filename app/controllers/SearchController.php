8f9b74d088ea6778825f29dbd547a64fa1ee9baf
hyy <376246826@qq.com>
2015-10-14 18:15:26 +0800

    Update & fix

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -205,7 +205,7 @@ class SearchController extends BaseController{
 
                         // 判断状态码
                     if ( $violations[ $sign ][ 'info' ][ 'status' ] == AgencyController::$AGENCY_STATUS_CONFIRMED ){
-                        return Response::json([ 'errCode' => 3, 'message' => '您已确认办理', 'sign' => $sign ]);
+                        return Response::json([ 'errCode' => 3, 'message' => '您已确认办理该车辆违章信息，请到提交订单页面确认或取消后再查询', 'sign' => $sign ]);
                     }
 
                     $service_fee = array_get( $violations, $sign.'.info.service_fee' );
