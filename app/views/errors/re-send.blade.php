6aeb95924c5b2aa19ed31954d7d4a95f719ef436
flyingalex <hulinalex@163.com>
2015-10-16 10:19:46 +0800

    修改邮箱失效页面

--- app/views/errors/re-send.blade.php
+++ app/views/errors/re-send.blade.php
@@ -1,28 +1,41 @@
-@extends("layouts.login-master")
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html, charset=utf8"/>
+	<title>
+		车尚 ｜ 链接失效
+	</title>
+	<link rel="stylesheet" type="text/css" href="/dist/css/components.css">
+	<link rel="stylesheet" type="text/css" href="/dist/css/pages/login.css">
+	<link rel="stylesheet" type="text/css" href="/dist/css/common.css">
+	<link rel="stylesheet" type="text/css" href="/dist/css/pages/register-b/email-active.css">
+	<link rel="stylesheet" type="text/css" href="/dist/css/pages/account-status/write-codes.css">
+</head>
+<body>
 
-@section("title")
-链接失效
-@stop
+	<!-- 页头 START		 -->
+	<!-- 页头 END		 -->
 
-@section("css")
-@parent
-<link rel="stylesheet" type="text/css" href="/dist/css/pages/register-b/email-active.css">
-<link rel="stylesheet" type="text/css" href="/dist/css/pages/account-status/write-codes.css">
-@stop
-
-@section("body")
-
-<div class="body-content">
-	
-	<div class="content-container">
-		<div class="email-tips write-codes-inputs no-pass-title">
-			该链接已失效
+	<div class="login-wrapper">
+		<div class="login-body clearfix">
+			<div class="body-content">
+				
+				<div class="content-container">
+					<div class="email-tips write-codes-inputs no-pass-title">
+						该链接已失效
+					</div>
+				</div>
 		</div>
-</div>
-
-@stop
-
-@section("js")
-	@parent
+	</div>
+	
+	<div class="login-footer">
+		<div class="footer-words">
+			<span>版权所有@广州车尚信息科技有限公司</span>|
+			<span>技术支持：广州紫睿科技有限公司</span>
+		</div>
+	</div>
+	<script type="text/javascript" src="/lib/js/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript" src="/dist/js/components.js"></script>
 	<script type="text/javascript" src="/dist/js/pages/register-b/email-active.js"></script>
-@stop
+</body>
+</html>
