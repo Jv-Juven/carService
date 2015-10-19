a4492077d0b5763cdd9a9f157c2d32488eb81c42
hyy <376246826@qq.com>
2015-10-19 11:41:43 +0800

    查询时新增车架号码后六位字段

---
+++ app/database/migrations/2015_10_19_113804_update_agency_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAgencyOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(){
+
+		Schema::table('agency_orders', function( $table ){
+
+			/*
+			 * 车架号码后六位
+			 */
+			$table->char( 'car_frame_no', 6 );
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(){
+
+		Schema::table('agency_orders', function(Blueprint $table){
+
+			$table->dropColumn( 'car_frame_no' );
+		});
+	}
+
+}
