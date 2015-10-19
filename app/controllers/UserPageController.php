037ca1e9a8f5c65725f43f6a07b16fc5ec28c377
flyingalex <hulinalex@163.com>
2015-10-19 10:51:49 +0800

    fix

--- app/controllers/UserPageController.php
+++ app/controllers/UserPageController.php
@@ -31,7 +31,7 @@ class UserPageController extends BaseController{
 			//登录后发邮件去邮箱验证邮箱
 			return View::make('errors.re-send');
 		}
-		$status = $user->status;
+		$status = User::find( $user->user_id )->status;
 		switch ( $status ) {
 				case 11:
 					return View::make('pages.register-b.reg-info');//信息登记
