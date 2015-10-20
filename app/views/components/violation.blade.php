1f935d375d6bd7fe7b91a0f8c5f346666898a9f0
jv-juven <juven-jv@foxmail.com>
2015-10-20 11:54:36 +0800

    修改“违章查询”结果显示标题头

--- app/views/components/violation.blade.php
+++ app/views/components/violation.blade.php
@@ -45,8 +45,7 @@
 		<div class="violation-records clearfix vio-records">
 			<input id="sign" type="hidden" value=""/>
 			<div class="vio-records-title">
-				车牌号<span class="records-plate">XXXXXX</span> | 发动机号<span class="records-engine"></span> | 车架号<span class="records-frame"></span>
-				共有<span class="records-total">3</span>笔违章记录
+				车牌号<span class="records-plate">XXXXXX</span> | 发动机号<span class="records-engine"></span> | 车架号<span class="records-frame"></span> <br/> 共有<span class="records-total">3</span>笔违章记录
 			</div>
 			<div class="vio-records-table vio-records-table01">
 				<table>
